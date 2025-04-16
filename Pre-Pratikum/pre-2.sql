b29eec9f7db6acc415ac53d6cd4bcc9058204a05
Xnowly <147676111+Xnowly@users.noreply.github.com>
2025-04-16 18:55:58 +0800

    Add files via upload

---
+++ Pre-Pratikum/pre-2.sql
@@ -0,0 +1,17 @@
+-- nama : Putra Ramadhan
+-- NIM : 23241091
+-- Kelas : C
+
+-- Melihat Data Base yang ada
+SHOW DATABASES;
+
+-- Melihat tabel dalam database mysql
+show TABLES;
+
+-- Mendeskripsikan sebuah table yang di pilih
+DESCRIBE user;
+
+-- melihat isi tabel user, khusus untuk kolom host ,user password
+SELECT Host, User, Password FROM user;
+
+
