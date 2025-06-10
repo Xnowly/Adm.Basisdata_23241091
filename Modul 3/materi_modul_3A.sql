494fa4c8a2c43aa982c423f860b607454a2d22a4
Xnowly <147676111+Xnowly@users.noreply.github.com>
2025-06-11 06:47:25 +0800

    materi_modul_3A.sql

--- Modul 3/materi_modul_3A.sql
+++ Modul 3/materi_modul_3A.sql
@@ -22,38 +22,38 @@ select nama_produk, qty from tr_penjualan_dqlab order by qty, nama_produk;
 select * from tr_penjualan_dqlab order by qty, tgl_transaksi;
 
 -- b. tampilkan semua kolom dari table ms pelanggan dengan mengurutkan berdasarkan nama_pelanggan
-select * from ms_pelanggan_dqlab order by nama_pelanggan;
+select * from ms_pelanggan order by nama_pelanggan;
 
 -- c. tampilkan semua kolom dari table ms pelanggan dengan mengurutkan berdasarkan alamat
-select * from ms_pelanggan_dqlab order by alamat;
+select * from ms_pelanggan order by alamat;
 
 
 -- ASC (DEFAULT/TIDAK WAJIB)(mengurutkan kecil ke besar), DESC (WAJIB DITULIS)(mengurutkan dari besar ke kecil)
 -- Praktek 3
-select nama_produk,qty from tr_penjualan_dqlab order by qty desc;
+select nama_produk,qty from tr_penjualan order by qty desc;
 
 -- Praktek 4
 -- Ambil nama produk, qty dari tr penjualan urut dengan desc dan asc
-select nama_produk,qty from tr_penjualan_dqlab order by qty desc, nama_produk asc;
+select nama_produk,qty from tr_penjualan order by qty desc, nama_produk asc;
 
 -- Latihan mandiri 2
 -- a. tampilkan semua kolom dari table tr penjualan dengan mengurutkan berdasarkan tgl transaksi secara DESC dan qty secara ASC
-select * from tr_penjualan_dqlab order by tgl_transaksi desc, qty asc;
+select * from tr_penjualan order by tgl_transaksi desc, qty asc;
 
 -- b. tampilkan semua kolom dari table ms_pelanggan dengan mengurutkan berdasarkan nama pelanggan secara DESC
-select * from ms_pelanggan_dqlab order by nama_pelanggan desc;
+select * from ms_pelanggan order by nama_pelanggan desc;
 
 -- c. tampilkan semua kolom dari table ms_pelanggan dengan mengurutkan berdasarkan alamat secara DESC
-select * from ms_pelanggan_dqlab order by alamat desc;
+select * from ms_pelanggan order by alamat desc;
 
 -- Praktik 5
 -- Menggunakan order by dari hasil perhitungan kolom
 -- Ambil nama produk, qty, harga dan perkalian qty*harga, urut berdasarkan qty*harga/total
-select nama_produk, qty, harga, qty*harga as total from tr_penjualan_dqlab order by total desc;
+select nama_produk, qty, harga, qty*harga as total from tr_penjualan order by total desc;
 
--- Latihan mandiri
+-- Latihan mandiri 3
 -- Pengurutan dengan ekspresi total harga menggunakan rumusan jumlah barang dikali harga barang dikurangi diskon
-select nama_produk, qty, harga, diskon_persen, (qty * harga - diskon_persen) as total_harga from tr_penjualan_dqlab order by total_harga desc;
+select nama_produk, qty, harga, diskon_persen, (qty * harga - diskon_persen) as total_harga from tr_penjualan order by total_harga desc;
 
 -- praktek 6
 -- WHERE dengan ORDER BY
@@ -64,23 +64,33 @@ FROM tr_penjualan
 WHERE nama_produk LIKE "F%"
 ORDER BY qty desc;
 
+-- latihan mandiri 4
+-- tampilkan semua kolom dari transaksi penjualan yang memilikki diskon dan urutkan dari harga yang tertinggi
+SELECT *
+FROM tr_penjualan
+WHERE diskon_persen > 0
+ORDER BY harga DESC;
+
+-- tampilkan kolom nama produk, kuantitas pembelian dan harga dari transaksi penjualan yang memiliki harga minimal seratus ribu rupiah dan diurutkan berdasarkan harga tertinggi
+SELECT nama_produk, qty, harga
+FROM tr_penjualan
+WHERE harga >= 100000
+ORDER BY harga DESC;
+
+-- tampilkan kolom nama produk, kuantitas pembelian dan harga dari transaksi penjualan yang memiliki harga minimal seratus ribu rupiah atau nama produk berawalan karakter 'T' dan diurutkan berdasarkan diskon tertinggi.
+SELECT nama_produk, qty, harga, diskon_persen
+FROM tr_penjualan
+WHERE harga >= 100000 OR nama_produk LIKE 'T%'
+ORDER BY diskon_persen DESC;
+
+
 -- PRAKTEK 7
 -- FUNGSI AGREGASI adalah fungsi untuk mengolah baris data/satu nilai baris baru
 -- DI GUNAKAN UNTUK MENGOLAH BEBERAPA BERIS DALAM KOLOM DATA
 -- meghitung jumlah(SUM), qty terjual PADDA TABEL PENJUALAN 
 SELECT qty FROM tr_penjualan;
-SELECT SUM(qty) FROM ms_pelanggan tr_penjualan -- ada 42 produk yang terjual
+SELECT SUM(qty) FROM tr_penjualan; -- ada 42 produk yang terjual
 
--- latihan mandiri
--- tampilkan semua kolom dari transaksi penjualan yang memilikki diskon dan urutkan dari harga yang tertinggi
-SELECT *
-FROM tr_penjualan
-WHERE diskon_persen
-ORDER BY harga DESC;
-
--- tampilkan kolom nama produk, kuantitas pembelian dan harga dari transaksi penjualan yang memiliki harga minimal seratus ribu rupiah dan diurutkan berdasarkan harga tertinggi
-
--- tampilkan kolom nama produk, kuantitas pembelian dan harga dari transaksi penjualan yang memiliki harga minimal seratus ribu rupiah atau nama produk berawalan karakter 'T dan diurutkan berdasarkan diskon tertinggi.
 
 -- praktek 8
 --- Hitung semua transaksi yang terjadi pada tb penjualan
@@ -123,7 +133,8 @@ SELECT nama_produk FROM tr_penjualan GROUP BY nama_produk;
 -- praktek 15 
 -- menampilkan nama produk dan qty dari hasil kelompok nama produk dan 
 SELECT nama_produk, qty
-FROM tr_penjualan , qty;
+FROM tr_penjualan
+GROUP BY nama_produk, qty;
 
 -- praktek 16
 -- tampilkan semua produk dan total qty terjual
